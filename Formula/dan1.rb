3c676126aecc1ab3aa29af5a8037f712b223d4fc
wz <oleksandr@tvori.info>
2024-12-07 15:45:52 +0200

    v0.1.0-stub

--- Formula/dan1.rb
+++ Formula/dan1.rb
@@ -1,25 +1,25 @@
-class YourApp < Formula
-  desc "DAN1 CLI"
+class Dan1 < Formula
+  desc "DAN 1"
   homepage "https://github.com/dan1tools/dan1"
   version "0.1.0"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/dan1tools/dan1/releases/download/v#{version}/dan1-macos-arm64.tar.gz"
-      sha256 "your-macos-arm64-binary-sha256-here"
+      sha256 "979eac5bab310400e5582d33de8024d0b2ee6788a72618e0e1583265c64e6e21"
     else
-      url "https://github.com/dan1tools/dan1/releases/download/v#{version}/dan1-macos-x86_64.tar.gz"
-      sha256 "your-macos-x86_64-binary-sha256-here"
+      url "https://github.com/dan1tools/dan1/releases/download/v#{version}/dan1-macos-x64.tar.gz"
+      sha256 "a50ee4db01ab00cc347fb5c9ff3c712a1ca834a7b1383e5c8c90a55ff2ce783c"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/dan1tools/dan1/releases/download/v#{version}/dan1-linux-arm64.tar.gz"
-      sha256 "your-linux-arm64-binary-sha256-here"
+      sha256 "5dee00a8ab122b652028e90c328ee55be31acc72f6156f2cb7173c45bc603321"
     else
-      url "https://github.com/dan1tools/dan1/releases/download/v#{version}/dan1-linux-x86_64.tar.gz"
-      sha256 "your-linux-x86_64-binary-sha256-here"
+      url "https://github.com/dan1tools/dan1/releases/download/v#{version}/dan1-linux-x64.tar.gz"
+      sha256 "bece0941f970d22b30fa51b19ec0e45539512b3e0f1c60aaddcc24b5456f6cad"
     end
   end
 
